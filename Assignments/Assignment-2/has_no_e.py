e800ca801e7cf6417cec804a79d1f33996217de8
Brian Jay <brian.jay1795@gmail.com>
2015-11-07 16:24:12 -0500

    Completed has_no_e.py

---
+++ Assignments/Assignment-2/has_no_e.py
@@ -0,0 +1,15 @@
+def has_no_e(search):
+	for i in search:
+		pass
+		if (i == 'e'):
+			print "true"
+			return
+	print "false"
+		
+def main():
+	file = open('gadsby.txt', "r") 
+	for line in file:
+		has_no_e(line);
+	file.close() 
+		
+main()
